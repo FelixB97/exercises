2f2481d32c6e9f33b6112766ccf5fe3c95cd73ea
felix <fillefish@hotmail.com>
2021-02-05 11:13:23 +0100

    added imputs js

--- basics.js
+++ basics.js
@@ -1,3 +1,20 @@
 "use strict";
 
 console.log("welcome");
+
+const boolean = true;
+
+const number = 24;
+
+const name = "Felix";
+
+const object = {
+    cats: 99,
+    lambo: 999
+};
+
+const nothing = null;
+
+let undefined;
+
+const symbol = symbol("symbol");
